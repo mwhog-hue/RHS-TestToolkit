7f8939c864abb5a3b9449489a581c4caeacd28fb
mwhog-hue <mweisse.hog@gmail.com>
2026-09-19 18:15:43 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'rh-flaeche-v2-4';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE)
+          .then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request)
+          .then(response => response || caches.match('./index.html'))
+      )
+  );
+});
